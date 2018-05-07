052ab15e73b482d38c0c95356086a2131aa7a513
ngalushko <galnikrom@gmail.com>
2018-05-07 21:04:57 +0300

    webapp; wip

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,12 +1,12 @@
 $(function(){
     var API_URL = "/api/v1/";
-	$("#login_form").on("submit", function(e) {
-		var arr = $(this).serializeArray(),
+	$("#submit_login").on("click", function(e) {
+		var arr = $("#login_form").serializeArray(),
 			data = {};
 		for (var i = 0; i < arr.length; i++) {
 			data[arr[i].name] = arr[i].value;
 		}
-        console.log(JSON.stringify(data));
+
         $.ajax({
             type: "POST",
             url: API_URL+"login",
@@ -14,14 +14,29 @@ $(function(){
             contentType: "application/json",
             processData: false
         }).done(function(d) {
-			document.cookie = "login" + "=" + escape(data['login']);
-			$("#login_container").hide();
-			$("#workplace").show();
-			loadRecords("emotions="+currentEmotion);
+			$("#login-modal").hide();
+            $("#workplace").show();
+            Cookies.set('token', d.token);
         }).fail(function (x) {
             alert(x);
         });
 
 		return false;
-	})
+    });
+
+    $("#send_link").on("click", function(e){
+        var link = $("#link_value").val();
+        $.ajax({
+            type: "GET",
+            url: API_URL+"audio",
+            data: "link="+link,
+            processData: false,
+            headers: {"Authorization": " BEARER " + Cookies.get('token')}
+        }).done(function(d) {
+            $('<div id="job_alert" class="alert alert-success" role="alert">'+ d.jobID +'</div>').insertBefore("#link_container");
+            $("#job_alert").delay(5000).slideUp('slow', function(){ $(this).remove(); } )
+        }).fail(function (x) {
+            //alert(x);
+        });
+    });
 });
